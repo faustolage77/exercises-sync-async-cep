ba46c0eed893ddce6ec5d2395fbd400bd70edc29
Felipe Rosas <contato@eufelipe.com>
2022-05-24 09:42:41 -0300

    chore: add unit tests

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 /*
-
 Para este exercício utilize o recurso de Async/Await do Javascript.
 Será necessário buscar as informações em uma api externa (ViaCep).
-Baixar o resultado, tratar o retorno. 
+Baixar o resultado, tratar o retorno para mostrar logradouro, bairro - localidade. 
+
+Exemplo de resultado obtido na api ViaCep:=
+    {
+      "cep": "01001-000",
+      "logradouro": "Praça da Sé",
+      "complemento": "lado ímpar",
+      "bairro": "Sé",
+      "localidade": "São Paulo",
+      "uf": "SP",
+      "ibge": "3550308",
+      "gia": "1004",
+      "ddd": "11",
+      "siafi": "7107"
+    }
+      
+Exemplo de como a função deve retornar: 
+
+"Praça da Sé, Sé - São Paulo"
 
 Documentação da API:  https://viacep.com.br/
-
 
 */
 
 const BASE_API_VIA_CEP = "https://viacep.com.br/ws/";
 
-function getCep(cep) {
-  return fetch(`${BASE_API_VIA_CEP}${cep}/json/`)
-    .then((response) => response.json())
-    .then((response) => {
-      if (response.erro) {
-        throw new Error(response.erro);
-      }
-      return response;
-    });
+/*
+    TODO 1:
+    - Implemente a função getAddressByCep com o recurso de Async/Await.
+    - A função deve retornar nesse formato: logradouro, bairro - localidade.
+    - A função deve retornar o erro `CEP não encontrado` caso o cep não seja encontrado.
+*/
+
+async function getAddressByCep(cep) {
+  // implemente aqui
 }
+
+module.exports = { getAddressByCep };
